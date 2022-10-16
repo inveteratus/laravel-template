ed38296126c46d7e6cae9b80dcd15590714eb21d
Alan <alan.mcfarlane@gmail.com>
2022-10-16 19:48:55 +0100

    Removed redundant check

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
                         <a href="#">Long Link</a>
                         <a href="#">Very Long Link</a>
                         <hr>
-                        @if (config('telescope.enabled') && Route::has('telescope') && Auth::user()->superuser)
+                        @if (Auth::user()->superuser && Route::has('telescope'))
                             <a href="{{ route('telescope') }} " target="{{ str(config('app.name') . ' Telescope')->slug() }}">Telescope</a>
                             <hr>
                         @endif
